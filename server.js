d79ad1115b194f26d18f073c45735064729fb1d0
Safiurrehmann <safiseventyguitar@gmail.com>
2025-03-13 09:43:49 +0500

    chnaged the server .listen function to exclude testing beacause was an open end

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
